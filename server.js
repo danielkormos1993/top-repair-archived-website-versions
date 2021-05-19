4c6397a1a65d619140f79193c7abd8436340f676
danielkormos1993 <danielkormos1993@gmail.com>
2021-05-19 10:35:49 +0200

    doing index page

---
+++ server.js
@@ -0,0 +1,17 @@
+﻿const express = require('express');
+const app = express();
+const cors = require('cors');
+
+//setup
+app.use(cors());
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+//base
+app.get('/', (req,res)=>{
+    res.render('layouts/layout.ejs', { page: 'index'});
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('TOP REPAIR Server has started successfully');
+});
