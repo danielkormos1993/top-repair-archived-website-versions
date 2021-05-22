b82a91a7ed4ef8824e7f6c46cef155b5903b8a05
danielkormos1993 <danielkormos1993@gmail.com>
2021-05-22 17:23:24 +0200

    prototype finished

--- server.js
+++ server.js
@@ -2,14 +2,59 @@
 const app = express();
 const cors = require('cors');
 
+const contactData = require('./public/cdn/data/contact.json');
+const reviews = require('./public/cdn/data/reviews.json');
+
 //setup
 app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+app.use(function(req, res, next){
+    res.locals.contactData = contactData;
+    res.locals.reviews = reviews;
+    next();
+});
+
 //base
 app.get('/', (req,res)=>{
     res.render('layouts/layout.ejs', { page: 'index'});
+});
+
+app.get('/jegkarjavitas', (req,res)=>{
+    res.render('layouts/layout.ejs', { page: 'hdr'});
+});
+
+app.get('/jegkarjavitas/cascojegkar', (req,res)=>{
+    res.render('layouts/layout.ejs', { page: 'hdr_casco'});
+});
+
+app.get('/jegkarjavitas/nemcascojegkar', (req,res)=>{
+    res.render('layouts/layout.ejs', { page: 'hdr_nemcasco'});
+});
+
+app.get('/horpadasjavitas', (req,res)=>{
+    res.render('layouts/layout.ejs', { page: 'pdr'});
+});
+
+app.get('/pdrtechnologia', (req,res)=>{
+    res.render('layouts/layout.ejs', { page: 'technology'});
+});
+
+app.get('/munkaink', (req,res)=>{
+    res.render('layouts/layout.ejs', { page: 'works'});
+});
+
+app.get('/rolunk', (req,res)=>{
+    res.render('layouts/layout.ejs', { page: 'aboutus'});
+});
+
+app.get('/gyik', (req,res)=>{
+    res.render('layouts/layout.ejs', { page: 'faq'});
+});
+
+app.get('/kapcsolat', (req,res)=>{
+    res.render('layouts/layout.ejs', { page: 'contact'});
 });
 
 app.listen(process.env.PORT || 3000, () => {
