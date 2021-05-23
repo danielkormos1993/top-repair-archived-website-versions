743a4d1c4386aba57b9af0f389584f6f06ccdd2d
danielkormos1993 <danielkormos1993@gmail.com>
2021-05-23 10:06:09 +0200

    check notes txt

--- server.js
+++ server.js
@@ -2,21 +2,28 @@
 const app = express();
 const cors = require('cors');
 
-const contactData = require('./public/cdn/data/contact.json');
-const reviews = require('./public/cdn/data/reviews.json');
-
-//setup
+// setup
 app.use(cors());
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+// data
+const pages = require('./public/cdn/data/pages.json');
+const contactData = require('./public/cdn/data/contact.json');
+const reviews = require('./public/cdn/data/reviews.json');
+
+const isProduction = process.env.mode === 'production';
+
 app.use(function(req, res, next){
     res.locals.contactData = contactData;
     res.locals.reviews = reviews;
+    res.locals.isProduction = isProduction;
+    res.locals.hostUrl = isProduction ? 'https://toprepair.hu' : 'http://192.168.0.101:3000';
+    res.locals.pages = pages;
     next();
 });
 
-//base
+// routes
 app.get('/', (req,res)=>{
     res.render('layouts/layout.ejs', { page: 'index'});
 });
@@ -55,6 +62,10 @@ app.get('/gyik', (req,res)=>{
 
 app.get('/kapcsolat', (req,res)=>{
     res.render('layouts/layout.ejs', { page: 'contact'});
+});
+
+app.get('/adatvedelem', (req,res)=>{
+    res.render('layouts/layout.ejs', { page: 'pp'});
 });
 
 app.get('*', (req,res)=>{
