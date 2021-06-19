7fc394d760a89de274dde11e85b94c963dc9cb34
danielkormos1993 <danielkormos1993@gmail.com>
2021-06-19 17:38:17 +0200

    added pageloader to this project

---
+++ dist/cdn/js/pageloader.js
@@ -0,0 +1,56 @@
+﻿let pageloaderStyle = document.createElement('style');
+pageloaderStyle.id = 'pageloader-style';
+pageloaderStyle.textContent = `
+
+    body{
+        min-height: 100vh;
+        margin: 0;
+        background-color: rgb(28, 28, 28);
+        color: rgb(204, 46, 40);
+    }
+
+    body.trbs-loader--active{
+        position: fixed;
+        overflow: hidden;
+        height: 100vh;
+        width: 100vw;
+    }
+
+    .trbs-loader--active:before{
+        content: '';
+        display: block;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        background-color: inherit;
+        z-index: 101;
+    }
+
+    .trbs-loader--active:after{
+        content: '';
+        display: block;
+        width: 1em;
+        height: 1em;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+        border: .2em solid transparent;
+        border-top: .2em solid currentColor;
+        border-radius: 50%;
+        animation: TrbsSpin 2s linear infinite;
+        z-index: 102;
+    }
+
+    @keyframes TrbsSpin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+    }
+
+`;
+
+document.head.appendChild(pageloaderStyle);
+
+window.addEventListener('load', () => document.body.classList.remove('trbs-loader--active'));
